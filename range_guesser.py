ac11ae838047b5f14d0be26c36a684c77154fe79
bik3te <leny.bueno@digital.security>
2018-10-18 11:04:24 +0200

    - Added a range cutter functionality

--- range_guesser.py
+++ range_guesser.py
@@ -1,5 +1,7 @@
 """
-Useful when you have no information regarding which ranges are used within the network
+Useful when:
+* You got some big subnets  - i.e. 10.232.0.0/16 - to cut into smaller ones
+* You have no information regarding which ranges are used within the network
 your are auditing.
 
 Generate the list of every first and last host's IP address of any
@@ -27,19 +29,22 @@ def usage():
     """Argument parser function."""
     parser = argparse.ArgumentParser(description='Hosts list generator to smartly identify subnets within your scope')
     parser.add_argument('-o', '--output', default='hosts.lst')
+    parser.add_argument('-s', '--subnet', help="If you get some big subnet - i.e. 10.232.0.0/16 - to cut into smaller ones", default=None)
     parser.add_argument('-n', '--netmask', type=int, default=24)
     parser.add_argument('--scan', help="Perform scanning after host list generation", action="store_true")
     parser.add_argument('-c', '--command', help="Specify your nmap command - i.e. nmap -oA subnets_guess -iL hosts.lst --top-ports=10", default='nmap -sn -oA subnets_guess -iL hosts.lst')
     return parser.parse_args()
 
-def gen_list(filename='hosts.lst', sub=24):
+def gen_list(filename='hosts.lst', sub=24, subnet=None):
     """Host list generator function."""
     ip_list = []
-    a_class = list(IPv4Network('10.0.0.0/8').subnets(new_prefix=sub))
-    b_class = list(IPv4Network('172.16.0.0/12').subnets(new_prefix=sub))
-    c_class = list(IPv4Network('192.168.0.0/16').subnets(new_prefix=sub))
-
-    subnet_list = a_class + b_class + c_class
+    if subnet:
+        subnet_list = list(IPv4Network(subnet).subnets(new_prefix=sub))
+    else:
+        a_class = list(IPv4Network('10.0.0.0/8').subnets(new_prefix=sub))
+        b_class = list(IPv4Network('172.16.0.0/12').subnets(new_prefix=sub))
+        c_class = list(IPv4Network('192.168.0.0/16').subnets(new_prefix=sub))
+        subnet_list = a_class + b_class + c_class
 
     for cpt, subnet in enumerate(subnet_list):
         # First host (removing network address)
@@ -60,6 +65,6 @@ def scan(nmap_command):
 
 if __name__ == '__main__':
     args = usage()
-    gen_list(args.output, args.netmask)
+    gen_list(args.output, args.netmask, args.subnet)
     if args.scan:
         scan(args.command)
